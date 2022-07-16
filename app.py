dfe5fdf13a206fd657ccfa8449833f4b47f44b62
JhonnyTeno <59611347+JhonnyTeno@users.noreply.github.com>
2022-07-16 12:50:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,81 +1,32 @@
-#!/usr/bin/python
+from flask import Flask, render_template, request
 
-try:
-	from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
-except:
-	from http.server import BaseHTTPRequestHandler,HTTPServer
-from os import curdir, sep
-try:
-	from urlparse import urlparse
-	from urlparse import urlparse, parse_qs
-except:
-	from urllib.parse import urlparse, parse_qs
+#import model
+import json
+import base64
+from urllib.parse import unquote
 
 import os
 port = int(os.environ.get("PORT", 5000))	
 PORT_NUMBER = port
 
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+@app.route("/")
+def root():
+	return render_template("index.html")
+
+@app.route("/index")
+def index():
+	return render_template("index.html")
 
 
-#This class will handles any incoming request from
-#the browser 
-class myHandler(BaseHTTPRequestHandler):
-	
-	#Handler for the GET requests
-	def do_GET(self):
-		path=self.path
-		print(self.path)
-		if self.path=="/":  #127.0.0.1:5000/
-			self.path="/index.html" #127.0.0.1:5000/index.html
-		try:
-			#Check the file extension required and
-			#set the right mime type
-
-			sendReply = False
-			if self.path.endswith(".html"):
-				mimetype='text/html'
-				sendReply = True
-			if self.path.endswith(".jpg"):
-				mimetype='image/jpg'
-				sendReply = True
-			if self.path.endswith(".gif"):
-				mimetype='image/gif'
-				sendReply = True
-			if self.path.endswith(".js"):
-				mimetype='application/javascript'
-				sendReply = True
-			if self.path.endswith(".css"):
-				mimetype='text/css'
-				sendReply = True
-
-			if sendReply == True:
-				#Open the static file requested and send it
-				#f = open(curdir + sep + self.path,'r') 
-				self.send_response(200)
-				self.send_header('Content-type',mimetype)
-				self.end_headers()
-				
-				try:
-					self.wfile.write('hola mundo TENORIOTO')
-				except:
-					self.wfile.write(bytes('hola mundo TENORITO', 'UTF-8'))
-				
-			return
+@app.route("/buscar")
+def buscar():
+	return render_template("buscar.html")
 
 
-		except IOError:
-			self.send_error(404,'File Not Found: %s' % self.path)
+if __name__ == "__main__":
+	app.run(host='0.0.0.0',port=PORT_NUMBER,debug = True)
 
-try:
-	#Create a web server and define the handler to manage the
-	#incoming request
-	server = HTTPServer(('0.0.0.0', PORT_NUMBER), myHandler)
-	print ('Started httpserver on port ' , PORT_NUMBER)
-	
-	#Wait forever for incoming htto requests
-	server.serve_forever()
 
-except KeyboardInterrupt:
-	print ('^C received, shutting down the web server')
-	server.socket.close()
 	
